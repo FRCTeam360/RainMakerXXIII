895af11cb99060b70cb32278080bb16a10e9a47c
lauren-lai <25lailauren@bprep.org>
2023-01-29 12:57:37 -0800

    made setUp() more readable

--- src/main/java/frc/robot/utils/ArmPoseCalculator.java
+++ src/main/java/frc/robot/utils/ArmPoseCalculator.java
@@ -19,31 +19,35 @@ public class ArmPoseCalculator {
 
   private Translation3d[][][] nodeCoordinates = new Translation3d[2][3][9]; //alliances, rows, nodes
   private double[] yCoordinates = new double[] {0, 1, 2, 3, 4, 5, 6, 7, 8}; //y coordinates for all the nodes, starting at the wall
+  private final int blue = 0;
+  private final int red = 1;
+
+  private final int bot = 0;
+  private final int mid = 1;
+  private final int top = 2;
 
   /** Creates a new ArmPoseCalculator. */
-  public ArmPoseCalculator() {
-
-  }
+  public ArmPoseCalculator() {}
 
   public void setUp() {
     for (int node = 0; node < 9; node++) { //sets all of the nodes going across all 3 grids to the same x and z values (all arbitrary)
-      nodeCoordinates[0][2][node] = new Translation3d(1, yCoordinates[node], 3); //moving across the field left to right
-      nodeCoordinates[0][1][node] = new Translation3d(2, yCoordinates[node], 2);
-      nodeCoordinates[0][0][node] = new Translation3d(3, yCoordinates[node], 1);
+      nodeCoordinates[blue][top][node] = new Translation3d(1, yCoordinates[node], 3); //moving across the field left to right
+      nodeCoordinates[blue][mid][node] = new Translation3d(2, yCoordinates[node], 2);
+      nodeCoordinates[blue][bot][node] = new Translation3d(3, yCoordinates[node], 1);
 
-      nodeCoordinates[1][0][node] = new Translation3d(4, yCoordinates[node], 1);
-      nodeCoordinates[1][1][node] = new Translation3d(5, yCoordinates[node], 2);
-      nodeCoordinates[1][2][node] = new Translation3d(6, yCoordinates[node], 3);
+      nodeCoordinates[red][bot][node] = new Translation3d(4, yCoordinates[node], 1);
+      nodeCoordinates[red][mid][node] = new Translation3d(5, yCoordinates[node], 2);
+      nodeCoordinates[red][top][node] = new Translation3d(6, yCoordinates[node], 3);
     }
 
     for (int node = 1; node <= 7; node += 3) { //new translation3d w a diff z value, same x and y values tho (REMEMBER TO UPDATE HERE TOO)
-      nodeCoordinates[0][2][node] = new Translation3d(1, yCoordinates[node], 2.5);
-      nodeCoordinates[0][1][node] = new Translation3d(2, yCoordinates[node], 1.5);
-      nodeCoordinates[0][0][node] = new Translation3d(3, yCoordinates[node], 0.5); 
+      nodeCoordinates[blue][top][node] = new Translation3d(1, yCoordinates[node], 2.5);
+      nodeCoordinates[blue][mid][node] = new Translation3d(2, yCoordinates[node], 1.5);
+      nodeCoordinates[blue][bot][node] = new Translation3d(3, yCoordinates[node], 0.5); 
       
-      nodeCoordinates[1][0][node] = new Translation3d(4, yCoordinates[node], 0.5);
-      nodeCoordinates[1][1][node] = new Translation3d(5, yCoordinates[node], 1.5);
-      nodeCoordinates[1][2][node] = new Translation3d(6, yCoordinates[node], 2.5);
+      nodeCoordinates[red][bot][node] = new Translation3d(4, yCoordinates[node], 0.5);
+      nodeCoordinates[red][mid][node] = new Translation3d(5, yCoordinates[node], 1.5);
+      nodeCoordinates[red][top][node] = new Translation3d(6, yCoordinates[node], 2.5);
     }
   }
   
