44f1946cba4d5b68a4fd9e563bc1448ea4e3b7bd
tbmilliren <60276487+tbmilliren@users.noreply.github.com>
2023-02-04 12:47:13 -0800

    tests are successful

--- src/test/java/ArmDistanceCalculationTests/ArmDistanceCalculationTests.java
+++ src/test/java/ArmDistanceCalculationTests/ArmDistanceCalculationTests.java
@@ -16,8 +16,8 @@ public class ArmDistanceCalculationTests {
     @Test
     public void test2dDistance_straightX1Y0Z0_distance1(){
         ArmPoseCalculator apc = new ArmPoseCalculator();
-        apc.setRobotPose(new Translation3d(0.0,0.0,0.0));
-        apc.setTargetPose(new Translation3d(1.0,0.0,0.0));
+        apc.setRobotTrans(new Translation3d(0.0,0.0,0.0));
+        apc.setTargetTrans(new Translation3d(1.0,0.0,0.0));
 
         double actual = apc.get2dDistance();
         
@@ -27,10 +27,10 @@ public class ArmDistanceCalculationTests {
     @Test
     public void test2dDistance_straightX1Y1Z0_distanceRoot2(){
         ArmPoseCalculator apc = new ArmPoseCalculator();
-        apc.setRobotPose(new Translation3d(0.0,0.0,0.0));
-        apc.setTargetPose(new Translation3d(1.0,1.0,0.0));
+        apc.setRobotTrans(new Translation3d(0.0,0.0,0.0));
+        apc.setTargetTrans(new Translation3d(1.0,1.0,0.0));
         
-        double expected = Math.sqrt(3.0);
+        double expected = Math.sqrt(2.0);
         double actual = apc.get2dDistance();
         
         assertEquals(expected,actual,"expected 2d distance to be " + expected + ", got " + actual);
@@ -38,8 +38,8 @@ public class ArmDistanceCalculationTests {
     @Test
     public void testTurretAngle_robotX0Y0Z03ArmX1Y0X03(){
         ArmPoseCalculator apc = new ArmPoseCalculator();
-        apc.setRobotPose(new Translation3d(0.0, 0.0, 0.3));
-        apc.setTargetPose(new Translation3d(1.0, 0.0, 0.3));
+        apc.setRobotTrans(new Translation3d(0.0, 0.0, 0.3));
+        apc.setTargetTrans(new Translation3d(1.0, 0.0, 0.3));
         double expected = 0.0; 
         double actual = apc.getElevationAngleDegrees();
         System.out.println(expected);
@@ -49,8 +49,8 @@ public class ArmDistanceCalculationTests {
     @Test
     public void testTurretAngle_straightX0Y0Z1_angle90(){
         ArmPoseCalculator apc = new ArmPoseCalculator();
-        apc.setRobotPose(new Translation3d(0.0, 0.0, 0.3));
-        apc.setTargetPose(new Translation3d(0.0, 0.0, 1.3));
+        apc.setRobotTrans(new Translation3d(0.0, 0.0, 0.3));
+        apc.setTargetTrans(new Translation3d(0.0, 0.0, 1.3));
         double expected = 90.0;
         double actual = apc.getElevationAngleDegrees();
         System.out.println(expected);
@@ -60,8 +60,8 @@ public class ArmDistanceCalculationTests {
     @Test
     public void testTurretAngle_straightX1Y0Z03_angle180(){
         ArmPoseCalculator apc = new ArmPoseCalculator();
-        apc.setRobotPose(new Translation3d(0.0, 0.0, 0.0));
-        apc.setTargetPose(new Translation3d(-1.0, 0.0, 0.0));
+        apc.setRobotTrans(new Translation3d(0.0, 0.0, 0.0));
+        apc.setTargetTrans(new Translation3d(-1.0, 0.0, 0.0));
         double expected = 0.0;
         double actual = apc.getElevationAngleDegrees();
         System.out.println(expected);
