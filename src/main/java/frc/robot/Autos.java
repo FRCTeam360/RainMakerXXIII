754a6ed045bf8e39a97b41850f282db0c6155bba
juliashimshock <24shimshockjulia@bprep.org>
2023-03-12 14:25:56 -0700

    auto changes

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -5,14 +5,12 @@
 package frc.robot;
 
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.commands.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import com.fasterxml.jackson.databind.ser.impl.FailingSerializer;
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
@@ -39,7 +37,7 @@ public final class Autos {
   private static DriveTrain driveTrain = DriveTrain.getInstance();
 
   // this is the auto chooser
-  private static SendableChooser<Command> autoChooser;
+  private static SendableChooser<AutoMode> autoChooser;
   // define autos here
   private static List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup("chop suey!",
       new PathConstraints(4, 3));
@@ -62,26 +60,30 @@ public final class Autos {
     }
   };
 
-  // instantiate autos here
-  private final Command example = autoBuilder.fullAuto(pathGroup);
-  private final Command notExample = autoBuilder.fullAuto(notPathGroup);
-
   public Autos() {
     autoChooser = new SendableChooser<>();
     // add auto options to chooser here
-    autoChooser.addOption("chop suey!", example);
-    autoChooser.addOption("lets go brandon", notExample);
-    autoChooser.addOption("tsla stock is good", getMyAuto());
-    autoChooser.addOption("mario broskito", getMyMarioAuto());
-    autoChooser.addOption("anywhere left", getAnywhereLeft());
-    autoChooser.addOption("anywhere right", getAnywhereRight());
-    autoChooser.addOption("engage from wall", getEngageFromWall());
-    autoChooser.addOption("engage from loading", getEngageFromLoading());
-    autoChooser.addOption("new 2 piece", getNew2Piece());
-    autoChooser.addOption("engage only", getEngageOnly());
-    autoChooser.addOption("null", null);
+    autoChooser.addOption("chop suey!", AutoMode.CHOP_SUEY);
+    autoChooser.addOption("lets go brandon", AutoMode.LETS_GO_BRANDON);
+    autoChooser.addOption("tsla stock is good", AutoMode.TSLA_STOCK_IS_GOOD);
+    autoChooser.addOption("mario broskito", AutoMode.MARIO_BROSKITO);
+    autoChooser.addOption("anywhere left", AutoMode.ANYWHERE_LEFT);
+    autoChooser.addOption("anywhere right", AutoMode.ANYWHERE_RIGHT);
+    autoChooser.addOption("engage from wall", AutoMode.ENGAGE_FROM_WALL);
+    autoChooser.addOption("engage from loading", AutoMode.ENGAGE_FROM_LOADING);
+    autoChooser.addOption("new 2 piece", AutoMode.NEW_2_PIECE);
+    autoChooser.addOption("engage only", AutoMode.ENGAGE_ONLY);
+    autoChooser.addOption("null", AutoMode.NULL);
 
     SmartDashboard.putData("Auto Chooser", autoChooser);
+  }
+
+  private Command getExample() {
+    return autoBuilder.fullAuto(pathGroup);
+  }
+
+  private Command getNotExample() {
+    return autoBuilder.fullAuto(notPathGroup);
   }
 
   private List<PathPlannerTrajectory> mirrorPathsForAlliance(List<PathPlannerTrajectory> mTrajectory) {
@@ -161,7 +163,7 @@ public final class Autos {
     // Command ShroomRidge = autoBuilder.followPath(luigi.get(2));
     return rainbowRoad.andThen(new InstantCommand(() -> driveTrain.setGyroOffset(180)))
         .andThen(coconutMall).andThen(new AutoEngage());// .andThen(DKJungle).andThen(ShroomRidge).andThen(new
-                                                                                  // AutoEngage());
+                                                        // AutoEngage());
   }
 
   private Command getMyAuto() {
@@ -177,87 +179,82 @@ public final class Autos {
     Command pathTSLA2 = autoBuilder.followPath(epicPathGroup.get(1));
 
     // return (stockMarketCrash
-    //     .andThen(new SetPositions(42, 1.05, -15, true)))
-    //     .andThen(new OpenClawCubeGround())
-    //     .andThen(new Homing())
-    //     .andThen(
-    //         new ParallelRaceGroup(
-    //             pathTSLA1,
-    //             new RunIntake(),
-    //             new SequentialCommandGroup(/* new SetPositions(90, 0.1, -180), */
-    //                 new SetPositions(0, 0.15, 180, true),
-    //                 // new WaitCommand(1),
-    //                 /* not working yet */
-    //                 new SetPositions(-25, 0.6, 180, true)),
-    //             new OpenClawCubeGround()))
-    //     .andThen(new PrintCommand("pathTSLA1 done"))
-    //     .andThen(
-    //         new ParallelRaceGroup((new WaitCommand(.25)), (new RunIntake()), (new OpenClawCubeGround(false))))
-    //     .andThen(
-    //         pathTSLA2.alongWith(new Homing())// .raceWith(new CloseClaw())
-    //     )
-    //     .andThen(new PrintCommand("pathTSLA2 done"))
-    //     .andThen(new SetPositions(42, 1.05, 15, true))
-    //     .andThen(
-    //         new ParallelRaceGroup(new OpenClawCubeGround(false), new RunIntakeReversed(), new WaitCommand(1)))
-    //     .andThen(new Homing());
+    // .andThen(new SetPositions(42, 1.05, -15, true)))
+    // .andThen(new OpenClawCubeGround())
+    // .andThen(new Homing())
+    // .andThen(
+    // new ParallelRaceGroup(
+    // pathTSLA1,
+    // new RunIntake(),
+    // new SequentialCommandGroup(/* new SetPositions(90, 0.1, -180), */
+    // new SetPositions(0, 0.15, 180, true),
+    // // new WaitCommand(1),
+    // /* not working yet */
+    // new SetPositions(-25, 0.6, 180, true)),
+    // new OpenClawCubeGround()))
+    // .andThen(new PrintCommand("pathTSLA1 done"))
+    // .andThen(
+    // new ParallelRaceGroup((new WaitCommand(.25)), (new RunIntake()), (new
+    // OpenClawCubeGround(false))))
+    // .andThen(
+    // pathTSLA2.alongWith(new Homing())// .raceWith(new CloseClaw())
+    // )
+    // .andThen(new PrintCommand("pathTSLA2 done"))
+    // .andThen(new SetPositions(42, 1.05, 15, true))
+    // .andThen(
+    // new ParallelRaceGroup(new OpenClawCubeGround(false), new RunIntakeReversed(),
+    // new WaitCommand(1)))
+    // .andThen(new Homing());
 
     return (stockMarketCrash
-    .andThen(new SetPositions(42, 1.05, -15, true))
-    .andThen(new OpenClawCubeGround(true))
-    .andThen(new Homing())
-    .andThen(new ParallelCommandGroup(
-      pathTSLA1, 
-      new SetPositions(0, 0.15, 180, true)
-    ))
-    );
+        .andThen(new SetPositions(42, 1.05, -15, true))
+        .andThen(new OpenClawCubeGround(true))
+        .andThen(new Homing())
+        .andThen(new ParallelCommandGroup(
+            pathTSLA1,
+            new SetPositions(0, 0.15, 180, true))));
   }
 
   private Command getNew2Piece() {
-    List<PathPlannerTrajectory> epicPathGroupInitial = PathPlanner.loadPathGroup("new 2 piece",
+    List<PathPlannerTrajectory> epicPathGroup = DriverStation.getAlliance() == Alliance.Red
+        ? mirrorPathsForAlliance(PathPlanner.loadPathGroup("new 2 piece",
+            new PathConstraints(2, 3)))
+        : PathPlanner.loadPathGroup("new 2 piece blue",
         new PathConstraints(2, 3));
     // for(int i = 0; i<epicPathGroup.size(); i++){
     // PathPlannerTrajectory.transformTrajectoryForAlliance(epicPathGroup.get(i),
     // Alliance.Red);
     // }
-    List<PathPlannerTrajectory> epicPathGroup = mirrorPathsForAlliance(epicPathGroupInitial);
     Command stockMarketCrash = autoBuilder.resetPose(epicPathGroup.get(0));
     Command part1 = autoBuilder.followPath(epicPathGroup.get(0));
     Command part2 = autoBuilder.followPath(epicPathGroup.get(1));
     Command part3 = autoBuilder.followPath(epicPathGroup.get(2));
 
     return (stockMarketCrash
-    .andThen(new SetPositions(42, 1.05, -15, true))
-    .andThen(new OpenClawCubeGround(true))
-    .andThen(new Homing())
-    .andThen(new ParallelCommandGroup(
-      part1, 
-      new SetPositions(0, 0.15, 180, true)
-      .andThen(new SetPositions(-25, 0.6, 180))
-    ))
-    .andThen(new ParallelRaceGroup(
-      part2, 
-      new RunIntake(),
-      new OpenClawCubeGround(false)
-    ))
-    .andThen(
-      new ParallelCommandGroup(
-        part3,
-        new Homing()
-      )
-    )
-    .andThen(
-      new SetPositions(42, 1.05, 15, true)
-    )
-    .andThen(
-      new ParallelRaceGroup(
-        new RunIntakeReversed(),
-        new WaitCommand(1)
-      )
-    )
-    .andThen(new Homing())
-    );
+        .andThen(new SetPositions(42, 1.05, -15, true))
+        .andThen(new OpenClawCubeGround(true))
+        .andThen(new Homing())
+        .andThen(new ParallelCommandGroup(
+            part1,
+            new SetPositions(0, 0.15, 180, true)
+                .andThen(new SetPositions(-26, 0.65, 180))))
+        .andThen(new ParallelRaceGroup(
+            part2,
+            new RunIntake(),
+            new OpenClawCubeGround(false)))
+        .andThen(
+            new ParallelCommandGroup(
+                part3,
+                new Homing()))
+        .andThen(
+            new SetPositions(42, 1.05, 15, true))
+        .andThen(
+            new ParallelRaceGroup(
+                new RunIntakeReversed(),
+                new WaitCommand(1)))
+        .andThen(new Homing()));
   }
+  
 
   private static SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
       driveTrain::getPose,
@@ -271,6 +268,35 @@ public final class Autos {
       driveTrain);
 
   public Command getAuto() {
-    return autoChooser.getSelected();
+    switch (autoChooser.getSelected()) {
+      case CHOP_SUEY:
+        return getExample();
+      case LETS_GO_BRANDON:
+        return getNotExample();
+      case TSLA_STOCK_IS_GOOD:
+        return getMyAuto();
+      case MARIO_BROSKITO:
+        return getMyMarioAuto();
+      case ANYWHERE_LEFT:
+        return getAnywhereLeft();
+      case ANYWHERE_RIGHT:
+        return getAnywhereRight();
+      case ENGAGE_FROM_WALL:
+        return getEngageFromWall();
+      case ENGAGE_FROM_LOADING:
+        return getEngageFromLoading();
+      case NEW_2_PIECE:
+        return getNew2Piece();
+      case ENGAGE_ONLY:
+        return getEngageOnly();
+      case NULL:
+      default:
+        return null;
+    }
+  }
+
+  private enum AutoMode {
+    CHOP_SUEY, LETS_GO_BRANDON, TSLA_STOCK_IS_GOOD, MARIO_BROSKITO, ANYWHERE_LEFT, ANYWHERE_RIGHT, ENGAGE_FROM_WALL,
+    ENGAGE_FROM_LOADING, NEW_2_PIECE, ENGAGE_ONLY, NULL
   }
 }
